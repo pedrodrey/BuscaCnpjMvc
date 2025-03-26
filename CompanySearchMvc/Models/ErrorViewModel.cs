bc05f17ffe0f976067c8206507fbdc99864d0560
Pedro Drey <pedrodrey2018@gmail.com>
2025-03-26 11:59:26 -0300

    Add project files.

---
+++ CompanySearchMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CompanySearchMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
